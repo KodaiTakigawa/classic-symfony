d87a3e6c92cd7eef969ec879759847a4dabf18b0
清水一貴 <s.kazutaka55555@gmail.com>
2018-07-31 11:14:40 +0900

    repositoryクラスを作った

---
+++ src/AppBundle/Repository/InquiryRepository.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yasushi_sakita
+ * Date: 16/02/20
+ * Time: 16:06
+ */
+
+namespace AppBundle\Repository;
+use Doctrine\ORM\EntityRepository;
+
+class InquiryRepository extends EntityRepository
+{
+    
+
+}
